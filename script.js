15d1f64dbe72cd60f8c4e297c075a4f6ee61e322
Fernando Lima <fernandoaslima@hotmail.com>
2025-01-13 16:59:38 -0300

    Botão de compartilhar

--- script.js
+++ script.js
@@ -8,28 +8,29 @@ if ("serviceWorker" in navigator) {
     console.warn("Service Worker não é suportado neste navegador.");
 }
 
-// document.getElementById("salvar-contato").addEventListener("click", function () {
-//     const vCardData = `
-//                     BEGIN:VCARD
-//                     VERSION:3.0
-//                     FN:Fernando Lima
-//                     ORG:Vórtice
-//                     TITLE:CEO
-//                     TEL;TYPE=work,voice;VALUE=uri:tel:+5532991144887
-//                     EMAIL:fernandoaslima@hotmail.com
-//                     URL:https://vorticehub.com.br/
-//                     ADR:;;Juiz de Fora-MG;;;;
-//                     END:VCARD
-//                             `;
+    document.getElementById("salvar-contato").addEventListener("click", function () {
+        const vCardData = `
+                BEGIN:VCARD
+                VERSION:3.0
+                FN:Fernando Lima
+                ORG:Vórtice
+                TITLE:CEO
+                TEL;TYPE=work,voice;VALUE=uri:tel:+5532991144887
+                EMAIL:fernandoaslima@hotmail.com
+                URL:https://vorticehub.com.br/
+                ADR:;;Juiz de Fora-MG;;;;
+                END:VCARD
+                        `;
 
-//     const blob = new Blob([vCardData], { type: "text/vcard" });
-//     const link = document.createElement("a");
-//     link.href = URL.createObjectURL(blob);
-//     link.download = "Fernando_Lima.vcf";
-//     link.click();
-// });
+        const blob = new Blob([vCardData], { type: "text/vcard" });
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "Fernando_Lima.vcf";
+        link.click();
+    });
 
-document.getElementById("salvar-contato").addEventListener("click", () => {
+
+document.getElementById("compartilhar-contato").addEventListener("click", () => {
     const contact = {
         name: "Fernando Lima",
         tel: "+5532991144887",
