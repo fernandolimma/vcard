72b8b393c32b629c693e31bd251b7da491020acc
Fernando Lima <fernandoaslima@hotmail.com>
2025-01-13 14:59:37 -0300

    Correção do apontamento do script

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("/service-worker.js")
-        .then(() => console.log("Service Worker registrado com sucesso"))
-        .catch(err => console.error("Erro ao registrar Service Worker:", err));
-}
-
-// Verificação Adicional Adicione uma verificação para alertar os usuários caso o navegador não suporte Service Workers
+// Verificação adicional para alertar caso o navegador não suporte Service Workers
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
         .register("/service-worker.js")
@@ -15,8 +8,27 @@ if ("serviceWorker" in navigator) {
     console.warn("Service Worker não é suportado neste navegador.");
 }
 
+// document.getElementById("salvar-contato").addEventListener("click", function () {
+//     const vCardData = `
+//                     BEGIN:VCARD
+//                     VERSION:3.0
+//                     FN:Fernando Lima
+//                     ORG:Vórtice
+//                     TITLE:CEO
+//                     TEL;TYPE=work,voice;VALUE=uri:tel:+5532991144887
+//                     EMAIL:fernandoaslima@hotmail.com
+//                     URL:https://vorticehub.com.br/
+//                     ADR:;;Juiz de Fora-MG;;;;
+//                     END:VCARD
+//                             `;
 
-// Script para a funcionalidade de salvar contato (NÂO FUNCIONOU)
+//     const blob = new Blob([vCardData], { type: "text/vcard" });
+//     const link = document.createElement("a");
+//     link.href = URL.createObjectURL(blob);
+//     link.download = "Fernando_Lima.vcf";
+//     link.click();
+// });
+
 document.getElementById("salvar-contato").addEventListener("click", () => {
     const contact = {
         name: "Fernando Lima",
@@ -32,12 +44,9 @@ document.getElementById("salvar-contato").addEventListener("click", () => {
             title: contact.name,
             text: `Nome: ${contact.name}\nOrganização: ${contact.organization}\nTelefone: ${contact.tel}\nEmail: ${contact.email}\nEndereço: ${contact.address}`,
         })
-        .then(() => console.log("Contato compartilhado com sucesso"))
-        .catch(err => console.error("Erro ao compartilhar contato:", err));
+            .then(() => console.log("Contato compartilhado com sucesso"))
+            .catch(err => console.error("Erro ao compartilhar contato:", err));
     } else {
         alert("Este dispositivo não suporta o recurso de compartilhamento!");
     }
-
-    
-    
 });
